09de95a668337da312877e5d725041602ba052ed
yogeshrbathe <ybathe@gmail.com>
2023-03-02 17:39:22 +0530

    From Date and To Date filter functionality screenshot & DB file added

--- enquiry db file/enquiry.sql
+++ enquiry db file/enquiry.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 01, 2023 at 02:34 PM
+-- Generation Time: Mar 02, 2023 at 01:07 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.2.32
 
@@ -31,6 +31,7 @@ CREATE TABLE `enquiry` (
   `id` int(11) NOT NULL,
   `name` varchar(100) DEFAULT NULL,
   `gender` varchar(10) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `phone` varchar(12) DEFAULT NULL,
   `enquiry` text DEFAULT NULL,
@@ -43,12 +44,13 @@ CREATE TABLE `enquiry` (
 -- Dumping data for table `enquiry`
 --
 
-INSERT INTO `enquiry` (`id`, `name`, `gender`, `email`, `phone`, `enquiry`, `source`, `updated_at`, `created_at`) VALUES
-(1, 'Yogesh', 'Male', 'ybathe@gmail.com', '1087672960', 'dfdf', 'banner', '2023-03-01 11:56:35', '2023-03-01 11:56:35'),
-(2, 'yog', 'Female', 'ybathe@gmail.com', '08087672960', 'test', 'banner', '2023-03-01 06:34:13', '2023-03-01 06:34:13'),
-(3, 'Yogesh', 'Female', 'ybathe@gmail.com', '8668632960', 'dfd', 'News Paper,Internet', '2023-03-01 06:44:12', '2023-03-01 06:44:12'),
-(4, 'Yogesh', 'Female', 'ybathe@gmail.com', '8668632960', 'dfdf', 'News Paper,Weekly News Paper,banner', '2023-03-01 07:32:03', '2023-03-01 07:32:03'),
-(5, 'test', 'Female', 'ybathe@gmail.com', '08087672960', 'sf dfdf', 'banner,Internet', '2023-03-01 07:33:26', '2023-03-01 07:33:26');
+INSERT INTO `enquiry` (`id`, `name`, `gender`, `birth_date`, `email`, `phone`, `enquiry`, `source`, `updated_at`, `created_at`) VALUES
+(1, 'Yogesh', 'Male', '2022-03-02', 'ybathe@gmail.com', '1087672960', 'dfdf', 'banner', '2023-03-01 11:56:35', '2023-03-01 11:56:35'),
+(2, 'Surekha', 'Female', '2023-12-01', 'ybathe@gmail.com', '08087672960', 'test', 'banner', '2023-03-01 06:34:13', '2023-03-01 06:34:13'),
+(3, 'Shravai', 'Female', '2021-01-01', 'shravani@gmail.com', '8668632960', 'dfd', 'News Paper,Internet', '2023-03-01 06:44:12', '2023-03-01 06:44:12'),
+(4, 'Ananya', 'Female', '1995-01-01', 'ananya@gmail.com', '7788995566', 'dfdf', 'News Paper,Weekly News Paper,banner', '2023-03-01 07:32:03', '2023-03-01 07:32:03'),
+(5, 'Jagjit', 'Male', '1980-01-01', 'jagjit@gmail.com', '9062504781', 'sf dfdf', 'banner,Internet', '2023-03-01 07:33:26', '2023-03-01 07:33:26'),
+(8, 'Nandini', 'Female', '2018-08-09', 'nandini@gmail.com', '8811223344', 'For Test Enquiry', 'Friend,Internet', '2023-03-02 05:59:06', '2023-03-02 05:59:06');
 
 --
 -- Indexes for dumped tables
@@ -68,7 +70,7 @@ ALTER TABLE `enquiry`
 -- AUTO_INCREMENT for table `enquiry`
 --
 ALTER TABLE `enquiry`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
